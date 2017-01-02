redhat-openstack/infrared

Add tripleo installer
FIXME: Port boostrap restore

This commit is documented in details in tripleo-undercloud.rst

Port sections of OSPD from master up to undercloud deployment.
Including images and backup

Change-Id: Ia715054186925598e20d4ee18f65e20b058064eb

plugins/tripleo-undercloud/plugin.spec
@@ -1,0 +1,139 @@
+plugin_type: install
+description: Install Tripleo on a designated UnderCloud node
+subparsers:
+    tripleo-undercloud:
+        # FIXME(yfried): duplicates "description"
+        help: Install Tripleo on a designated UnderCloud node
+        include_groups: ["Ansible options", "Inventory", "Common options", "Answers file"]
+        groups:
+            - title: Undercloud Configuration
+              options:
+                  config-file:
+                      type: Value
+                      help: |
+                          Path to a custom undercloud.conf file to use for deployment.
+                          If not set, it will look under `templates` path for a file named `undercloud.conf`.
+                          If no `undercloud.conf` file found, it will use default `/usr/share/instack-undercloud/undercloud.conf.sample`
+                          that is provided by the installation.
+
+                  config-options:
+                      type: KeyValueList
+                      help: |
+                          Forces additional Undercloud configuration (undercloud.conf) options.
+                          Format: --config-options="section.option=value1;section.option=value".
+
+                  ssl:
+                      type: Bool
+                      help: |
+                          Specifies whether ths SSL should be used for undercloud
+                          A self-signed SSL cert will be generated.
+                      default: no
+
+            - title: Setup Tripleo Packages
+              options:
+                  mirror:
+                      type: Value
+                      help: |
+                          Enable usage of specified mirror (for rpm, pip etc) [brq,qeos,tlv - or hostname].
+                          (Specified mirror needs to proxy multiple rpm source hosts and pypi packages.)
+                      default: ''
+
+                  version:
+                      type: Value
+                      help: |
+                          The product version (product == director)
+                          Numbers are for OSP releases
+                          Names are for RDO releases
+                      choices:
+                        - "7"
+                        - "8"
+                        - "9"
+                        - "10"
+                        - "11"
+                        - kilo
+                        - liberty
+                        - mitaka
+                        - newton
+                        - ocata
+                      required: true
+
+                  build:
+                      help: |
+                          "String represents a timestamp of the OSP puddle
+                          (for the given product core version).
+                          Examples: 'latest', '2016-08-11.1'"
+                      type: Value
+                      default: latest
+
+            - title: Tripleo User
+              options:
+                  user-name:
+                      type: Value
+                      help: The installation user name. Will be generated if missing
+                      default: stack
+
+                  user-password:
+                      type: Value
+                      help: The installation user password
+                      default: stack
+
+            - title: Custom Repositories
+              options:
+                  repos-config:
+                      type: Value
+                      help: |
+                          YAML file
+                          define new repositories or update exsiting according to file.
+                          see documentaion for more details
+                  repos-urls:
+                      type: Value
+                      help: |
+                          comma separated list of URLs to download repo files to ``/etc/yum.repos.d``
+
+            - title: Overcloud images
+              options:
+                  images-task:
+                      type: Value
+                      help: |
+                          Specifies the source for the OverCloud images:
+                          * RPM - packaged with product (versions 8 and above)
+                          * IMPORT - fetch from external source (versions 7 and 8). Requires to specify '--image-url'.
+                          * BUILD - build images locally (takes longer)
+                      choices:
+                          - rpm
+                          - import
+                          - build
+
+                  images-url:
+                      type: Value
+                      help: |
+                          Specifies the import image url.
+                          Required when images task is 'import'
+                          When images task is 'build', this is an optional source for base_image to start from.
+                      required_when: "images-task == import"
+
+                  images-repos:
+                      type: Bool
+                      help: |
+                          Update OverCloud image with repo details.
+                      default: yes
+
+                  images-packages:
+                      type: Value
+                      help: |
+                          List of packages to install seperated by commas.
+                          Requires ``--images-repos=yes``
+                          Example: vim,git
+
+                  images-cleanup:
+                      type: Bool
+                      help: |
+                          Removes all the downloaded images when images-task is in 'rpm' or 'import'
+                      default: yes
+
+            - title: Backup
+              options:
+                  backup:
+                      type: Bool
+                      help: backup undercloud image to use for bootstrap
+                      default: no

rhos-infra/tripleo-undercloud

plugin.spec
@@ -1,0 +1,139 @@
+plugin_type: install
+description: Install Tripleo on a designated undercloud node
+subparsers:
+    tripleo-undercloud:
+        # FIXME(yfried): duplicates "description"
+        help: Install Tripleo on a designated undercloud node
+        include_groups: ["Ansible options", "Inventory", "Common options", "Answers file"]
+        groups:
+            - title: Undercloud Configuration
+              options:
+                  config-file:
+                      type: Value
+                      help: |
+                          Path to a custom undercloud.conf file to use for deployment.
+                          If not set, it will look under `templates` path for a file named `undercloud.conf`.
+                          If no `undercloud.conf` file found, it will use default `/usr/share/instack-undercloud/undercloud.conf.sample`
+                          that is provided by the installation.
+
+                  config-options:
+                      type: KeyValueList
+                      help: |
+                          Forces additional Undercloud configuration (undercloud.conf) options.
+                          Format: --config-options="section.option=value1;section.option=value".
+
+                  ssl:
+                      type: Bool
+                      help: |
+                          Specifies whether ths SSL should be used for undercloud
+                          A self-signed SSL cert will be generated.
+                      default: no
+
+            - title: Setup Undercloud Packages
+              options:
+                  mirror:
+                      type: Value
+                      help: |
+                          Enable usage of specified mirror (for rpm, pip etc) [brq,qeos,tlv - or hostname].
+                          (Specified mirror needs to proxy multiple rpm source hosts and pypi packages.)
+                      default: ''
+
+                  version:
+                      type: Value
+                      help: |
+                          The product version (product == director)
+                          Numbers are for OSP releases
+                          Names are for RDO releases
+                      choices:
+                        - "7"
+                        - "8"
+                        - "9"
+                        - "10"
+                        - "11"
+                        - kilo
+                        - liberty
+                        - mitaka
+                        - newton
+                        - ocata
+                      required: true
+
+                  build:
+                      help: |
+                          "String represents a timestamp of the OSP puddle
+                          (for the given product core version).
+                          Examples: 'latest', '2016-08-11.1'"
+                      type: Value
+                      default: latest
+
+            - title: Tripleo User
+              options:
+                  user-name:
+                      type: Value
+                      help: The installation user name. Will be generated if missing
+                      default: stack
+
+                  user-password:
+                      type: Value
+                      help: The installation user password
+                      default: stack
+
+            - title: Custom Repositories
+              options:
+                  repos-config:
+                      type: Value
+                      help: |
+                          YAML file
+                          define new repositories or update exsiting according to file.
+                          see documentaion for more details
+                  repos-urls:
+                      type: Value
+                      help: |
+                          comma separated list of URLs to download repo files to ``/etc/yum.repos.d``
+
+            - title: Overcloud images
+              options:
+                  images-task:
+                      type: Value
+                      help: |
+                          Specifies the source for the OverCloud images:
+                          * RPM - packaged with product (versions 8 and above)
+                          * IMPORT - fetch from external source (versions 7 and 8). Requires to specify '--image-url'.
+                          * BUILD - build images locally (takes longer)
+                      choices:
+                          - rpm
+                          - import
+                          - build
+
+                  images-url:
+                      type: Value
+                      help: |
+                          Specifies the import image url.
+                          Required when images task is 'import'
+                          When images task is 'build', this is an optional source for base_image to start from.
+                      required_when: "images-task == import"
+
+                  images-repos:
+                      type: Bool
+                      help: |
+                          Update OverCloud image with repo details.
+                      default: yes
+
+                  images-packages:
+                      type: Value
+                      help: |
+                          List of packages to install seperated by commas.
+                          Requires ``--images-repos=yes``
+                          Example: vim,git
+
+                  images-cleanup:
+                      type: Bool
+                      help: |
+                          Removes all the downloaded images when images-task is in 'rpm' or 'import'
+                      default: yes
+
+            - title: Backup
+              options:
+                  backup:
+                      type: Bool
+                      help: backup undercloud image to use for bootstrap
+                      default: no